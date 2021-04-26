0297924bda58ff7246a4e2ac94a481c8dd6b27dc
slamdunk7575 <slamdunk7575@gmail.com>
2021-04-26 22:08:13 +0900

    (feat) init commit

---
+++ src/test/java/me/toy/atdd/subwayadmin/AtddSubwayAdminToyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.toy.atdd.subwayadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtddSubwayAdminToyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
