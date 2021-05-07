6d709a480b29bf7de3c9949acec68642a04df007
slamdunk7575 <slamdunk7575@gmail.com>
2021-05-08 01:00:49 +0900

    (feat) 커스텀 예외 정의

---
+++ src/main/java/me/toy/atdd/subwayadmin/exception/StationNotExistException.java
@@ -0,0 +1,8 @@
+package me.toy.atdd.subwayadmin.exception;
+
+public class StationNotExistException extends SubwayException {
+
+    public StationNotExistException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/me/toy/atdd/subwayadmin/exception/SubwayException.java
@@ -0,0 +1,8 @@
+package me.toy.atdd.subwayadmin.exception;
+
+public class SubwayException extends RuntimeException {
+
+    public SubwayException(String message) {
+        super(message);
+    }
+}
