e2ce43c1c320566a76570188682a51d85eabecc8
slamdunk7575 <slamdunk7575@gmail.com>
2021-05-08 03:25:04 +0900

    (refactor) Distance 수정

--- src/main/java/me/toy/atdd/subwayadmin/section/domain/Distance.java
+++ src/main/java/me/toy/atdd/subwayadmin/section/domain/Distance.java
@@ -12,18 +12,33 @@ import javax.persistence.Embeddable;
 public class Distance {
 
     private static final String INVALID_VALUE_ERROR_MESSAGE = "거리는 0보다 커야 합니다.";
+    private static final String INVALID_DISTANCE_ERROR_MESSAGE = "구간을 등록시 기존 역 사이 길이보다 크거나 같으면 등록을 할 수 없습니다.";
     private static final int ZERO = 0;
 
-    private int distance;
+    private int value;
 
     public Distance(int distance) {
-        validateDistance(distance);
-        this.distance = distance;
+        validateInitDistance(distance);
+        this.value = distance;
     }
 
-    private void validateDistance(int distance) {
-        if (distance <= ZERO) {
+    private void validateInitDistance(int distance) {
+        if (distance < ZERO) {
             throw new IllegalArgumentException(INVALID_VALUE_ERROR_MESSAGE);
+        }
+    }
+
+    public void updateDistance(int newDistance) {
+        if (this.value != 0) {
+            validateDistance(newDistance);
+            this.value -= newDistance;
+        }
+        this.value = newDistance;
+    }
+
+    private void validateDistance(int newDistance) {
+        if (this.value <= newDistance) {
+            throw new IllegalArgumentException(INVALID_DISTANCE_ERROR_MESSAGE);
         }
     }
 }
